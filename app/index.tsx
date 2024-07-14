3f69570f7d216aab981eab50687a2c3f5a1b7608
Minh Nguyen <thn649@usask.ca>
2024-07-14 11:32:07 -0600

    custom carousel works LFGG

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, Dimensions } from "react-native";
 import Carousel from "react-native-reanimated-carousel";
+import APCarousel from "@/components/APCarousel";
 
 export default function Index() {
   const width = Dimensions.get('window').width;
@@ -12,28 +13,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Carousel
-                loop
-                width={width}
-                height={width / 2}
-                autoPlay={true}
-                data={[...new Array(6).keys()]}
-                scrollAnimationDuration={1000}
-                onSnapToItem={(index) => console.log('current index:', index)}
-                renderItem={({ index }) => (
-                    <View
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                            {index}
-                        </Text>
-                    </View>
-                )}
-            />
+      <APCarousel/>
     </View>
   );
 }
---
+++ components/APCarousel.tsx
@@ -0,0 +1,40 @@
+import { Dimensions, View, Image, StyleSheet } from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
+import {useState} from "react";
+
+export default function APCarousel() {
+    const width = Dimensions.get('window').width;
+    const [banner] = useState([
+        require('../assets/images/jose_wallpaper.png'),
+        require('../assets/images/p3r-all-out-cards.png'),
+        require('../assets/images/the gang.png'),
+    ])
+    return (
+        <Carousel
+        loop
+        width={width}
+        height={width/2}
+        autoPlay={true}
+        data={banner}
+        scrollAnimationDuration={1000}
+        renderItem={({item, index}) => (
+            <View style={styles.imageContainer}>
+                <Image source={item} key={index} style={styles.image}/>
+            </View>
+            
+        )}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width / 2,
+    },
+    imageContainer: {
+        flex: 1,
+        borderWidth:.5,
+        justifyContent: 'center'
+    }
+})
