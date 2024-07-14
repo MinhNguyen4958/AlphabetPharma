18a0e284154742de5c79adc3830ee154738665d7
Minh Nguyen <thn649@usask.ca>
2024-07-14 10:50:13 -0600

    default carousel works

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
-import { Text, View } from "react-native";
+import { Text, View, Dimensions } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
 
 export default function Index() {
+  const width = Dimensions.get('window').width;
   return (
     <View
       style={{
@@ -10,6 +12,28 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Carousel
+                loop
+                width={width}
+                height={width / 2}
+                autoPlay={true}
+                data={[...new Array(6).keys()]}
+                scrollAnimationDuration={1000}
+                onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={({ index }) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            borderWidth: 1,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                            {index}
+                        </Text>
+                    </View>
+                )}
+            />
     </View>
   );
 }
